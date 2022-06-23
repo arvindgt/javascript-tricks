68bd1b09217c421acc999886b5d85d11b166faad
Arvind Kushwaha <arvindkshwh939@gmail.com>
2022-06-23 17:15:20 +0530

    Example 6

--- 06.js
+++ 06.js
@@ -4,6 +4,10 @@ function count() { return 20; }
 
 console.log(typeof count); // number
 
-console.log(count);
+console.log(count); // 10
 
-console.log(count());
+console.log(count()); // TypeError: count is not a function
+
+/**
+ * because of Hoisting
+ */
